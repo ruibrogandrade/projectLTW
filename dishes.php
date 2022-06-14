b3ce1df582913c0fc507a735da302630364ccb3b
manueljamorim <manueljoaoamorim@gmail.com>
2022-06-14 19:29:11 +0100

    Scroll tweak iphone restaurants

--- dishes.php
+++ dishes.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="CSS/style_all.css">
     <link rel="stylesheet" href="CSS/style_restaurants.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body  class="mainpage">
   <header>
